fa434c43021cdae225f5a9a4f4246e3f50080c6a
mbialowas10 <mbialowas@rrc.ca>
2023-03-08 14:33:59 -0600

    initial commit

---
+++ db/seeds.rb
@@ -0,0 +1,8 @@
+100.times do
+  Quote.create(
+    character: Faker::TvShows::Seinfeld.character,
+    content: Faker::TvShows::Seinfeld.quote
+  )
+end
+
+puts "Added #{Quote.count} quotes from Seinfeld."
